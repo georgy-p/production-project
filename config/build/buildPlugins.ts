369bf0e6d551d951d2ae0074a6e571c74cc2bda2
Georgy <pigargeorge@gmail.com>
2023-09-13 19:49:08 +0300

    add MiniCssExtractPlugin

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -2,6 +2,7 @@ import path from "path";
 import webpack from "webpack";
 import HTMLWebpackPlugin from 'html-webpack-plugin';
 import { BuildOptions } from "./types/config";
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
 
 export function buildPlugins( { paths }: BuildOptions): webpack.WebpackPluginInstance[] {
     
@@ -9,6 +10,10 @@ export function buildPlugins( { paths }: BuildOptions): webpack.WebpackPluginIns
         new HTMLWebpackPlugin({
             template: paths.html,
           }),
-          new webpack.ProgressPlugin(),
+        new webpack.ProgressPlugin(),
+        new MiniCssExtractPlugin({
+            filename: 'css/[name].[contenthash:8].css',
+            chunkFilename: 'css/[name].[contenthash:8].css',
+        })
     ]
 }
