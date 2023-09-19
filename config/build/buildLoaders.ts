45b29a12051dd26f525e2e7498e47ed58800e555
George <81578359+georgy-p@users.noreply.github.com>
2023-09-19 17:46:06 +0300

    add babel

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -3,6 +3,16 @@ import MiniCssExtractPlugin from "mini-css-extract-plugin";
 import {BuildOptions} from "./types/config";
 
 export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
+    const  babelLoader = {
+                test: /\.(js|jsx|tsx$)/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            }
 
     const svgLoader = {
         test: /\.svg$/,
@@ -44,6 +54,7 @@ export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
     }
     
     return [
+        babelLoader,
         typescriptLoader,
         cssLoader,
         fileLoader,
